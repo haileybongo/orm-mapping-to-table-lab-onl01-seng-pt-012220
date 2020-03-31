acc26a8773442f59e11b566aae12effaf0e0f705
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 22:43:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  def initialize(name, grade)
+  attr_accessor :name, :id, :grade 
+  
+  def initialize(name, grade, id = nil)
+    @id = id
     @name = name
     @grade = grade
   end
