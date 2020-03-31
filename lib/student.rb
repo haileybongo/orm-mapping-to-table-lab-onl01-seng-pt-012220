a528c53922ed967b0c402fc1be1e2d92f4af5236
Hailey Bongo <haileyebongo@gmail.com>
2020-03-31 22:58:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,9 +35,9 @@ class Student
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
-    SQL
+      SQL
  
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
  
